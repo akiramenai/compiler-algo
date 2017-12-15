b32d6df985af142d94bca1ec526d16b14cafc364
Pavel Kopyl <p.kopyl@samsung.com>
2017-12-15 21:53:26 +0300

    Add Linux/GCC-6 support.

--- include/compatibility.h
+++ include/compatibility.h
@@ -6,11 +6,7 @@
 #ifndef COMPATIBILITY_H
 #define COMPATIBILITY_H
 
-#if __has_include(<variant>) && !defined(USE_BOOST_CONTAINERS)
-#include <variant>
-#else
 #include <boost/variant.hpp>
-#endif
 #include <iostream>
 #if __has_include(<optional>) && !defined(USE_BOOST_CONTAINERS)
 #include <optional>
@@ -19,20 +15,13 @@
 #endif
 #if __has_include(<string_view>) && !defined(USE_BOOST_CONTAINERS)
 #include <string_view>
+#elif __has_include(<experimental/string_view>) && !defined(USE_BOOST_CONTAINERS)
+#include <experimental/string_view>
 #else
 #include <boost/utility/string_view.hpp>
 #endif
 
 namespace wyrm {
-#if __has_include(<variant>) && !defined(USE_BOOST_CONTAINERS)
-template <typename... Args> using variant = std::variant<Args...>;
-template <typename... ArgTys> inline auto visit(ArgTys &&... Args) {
-  return std::visit(std::forward<ArgTys>(Args)...);
-};
-template <typename T, typename... ArgTys> inline auto get(ArgTys &&... Args) {
-  return std::get<T>(std::forward<ArgTys>(Args)...);
-};
-#else
 template <typename... Args> using variant = boost::variant<Args...>;
 template <typename... ArgTys> inline auto visit(ArgTys &&... Args) {
   return boost::apply_visitor(std::forward<ArgTys>(Args)...);
@@ -40,7 +29,6 @@ template <typename... ArgTys> inline auto visit(ArgTys &&... Args) {
 template <typename T, typename... ArgTys> inline auto get(ArgTys &&... Args) {
   return boost::get<T>(std::forward<ArgTys>(Args)...);
 };
-#endif
 
 #if __has_include(<optional>) && !defined(USE_BOOST_CONTAINERS)
 template <typename ValueT> using optional = std::optional<ValueT>;
@@ -50,6 +38,8 @@ template <typename ValueT> using optional = boost::optional<ValueT>;
 
 #if __has_include(<string_view>) && !defined(USE_BOOST_CONTAINERS)
 using string_view = std::string_view;
+#elif __has_include(<experimental/string_view>) && !defined(USE_BOOST_CONTAINERS)
+using string_view = std::experimental::string_view;
 #else
 using string_view = boost::string_view;
 #endif
--- src/context.cpp
+++ src/context.cpp
@@ -1,6 +1,8 @@
 #include "context.h"
 
 namespace wyrm {
+WyrmContext GlobalContext{};
+
 string_view internedName(std::string &&name) {
   auto NameIt =
       std::get<0>(GlobalContext.StringStorage.insert(std::move(name)));
